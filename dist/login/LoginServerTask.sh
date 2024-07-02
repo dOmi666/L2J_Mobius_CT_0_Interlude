cb27f3a58052a3e934de94c9bf0a9a58a7c4abdf
dOmi666 <wolkanakar@gmail.com>
2024-07-02 14:47:06 +0300

    hotfix: GUI activated again

--- dist/login/LoginServerTask.sh
+++ dist/login/LoginServerTask.sh
@@ -6,7 +6,7 @@ do
 	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
 	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
 	#when using log on below this line, login cannot be started 
-	java -Djava.awt.headless=true $(cat "java.cfg") -jar ../libs/LoginServer.jar #> log/stdout.log 2>&1
+	java -Djava.awt.headless=false $(cat "java.cfg") -jar ../libs/LoginServer.jar #> log/stdout.log 2>&1
 	err=$?
 	sleep 10;
 done
